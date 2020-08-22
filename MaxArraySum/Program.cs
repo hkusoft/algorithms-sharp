8105559a578241458cfb071941dee48b0a3abf83
David <h0195526@connect.hku.hk>
2020-08-23 00:43:00 +0800

    Add Max Array Sum and Testing project

---
+++ MaxArraySum/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MaxArraySum
+{
+    public class Program
+    {
+        public static int MaxSubsetSum(List<int> list)
+        {
+            if (list.Count <= 2)
+                return list.Max();
+
+            if (list.Count == 3)
+            {
+                var sum13 = list[0] + list[2];
+                return Math.Max(list.Max(), sum13);
+            }
+
+            var last = list.Last();
+
+            var list0 = list.Take(list.Count - 1).ToList();
+            var max0 = MaxSubsetSum(list0);
+            var max1 = Math.Max(max0, last);
+
+            var list1 = list0.Take(list0.Count - 1).ToList();
+            var max2 = MaxSubsetSum(list1);
+
+            return Math.Max(max1, max2 + last);
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            var list = new List<int>() { 2, 1, 5 };
+            Console.WriteLine(MaxSubsetSum(list));  //7
+
+            list = new List<int>() { 2, 1 };
+            Console.WriteLine(MaxSubsetSum(list));  //2
+
+            list = new List<int>() { 1 };
+            Console.WriteLine(MaxSubsetSum(list));  //1
+
+            list = new List<int>() { 2, 1, 5, 8 };
+            Console.WriteLine(MaxSubsetSum(list));  //10
+
+            list = new List<int>() { 2, 1, 5, 8, 4 };
+            Console.WriteLine(MaxSubsetSum(list));  //11
+
+        }
+    }
+}
---
+++ MaxArraySumTests/ProgramTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MaxArraySum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaxArraySum.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void MaxSubsetSumTest()
+        {
+            var list = new List<int>() {2, 1, 5};
+            Assert.AreEqual(Program.MaxSubsetSum(list), 7);
+
+            list = new List<int>() { 2, 1 };
+            Assert.AreEqual(Program.MaxSubsetSum(list), 2);
+
+            list = new List<int>() { 1 };
+            Assert.AreEqual(Program.MaxSubsetSum(list), 1);
+
+            list = new List<int>() {2, 1, 5, 8};
+            Assert.AreEqual(Program.MaxSubsetSum(list), 10);
+
+            list = new List<int>() {2, 1, 5, 8, 4};
+            Assert.AreEqual(Program.MaxSubsetSum(list), 11);
+
+
+        }
+    }
+}
